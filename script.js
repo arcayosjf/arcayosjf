138930701ea23b097ab6a1622108f5bba83932f4
arcayosjf <108864293+arcayosjf@users.noreply.github.com>
2022-07-07 01:20:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//open and close navigation
+function openMenu (){
+    document.getElementById('dropdown').style.marginTop = "0px";
+}
+
+function closeMenu (){
+    document.getElementById('dropdown').style.marginTop = "-52vh";
+}
